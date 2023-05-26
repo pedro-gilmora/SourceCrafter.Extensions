594d6f4a8850989654a8761ecac91917c43b66dc
pedro.gilmora <pedro.gilmora@outlook.es>
2023-05-26 08:52:13 -0400

    Initialized

---
+++ SourceCrafter.Extensions.Collections/CollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿#nullable enable
+using System.Collections.Generic;
+
+namespace SourceCrafter;
+
+public static class CollectionExtensions
+{
+    public static TList AddNested<TList, TKey, TValueItem>(this Dictionary<TKey, TList> listHash, TKey key, TValueItem valueItem)
+        where TList : ICollection<TValueItem>, new()
+    {
+        if (listHash.TryGetValue(key, out var valueItems))
+            valueItems.Add(valueItem);
+        else
+            listHash.Add(key, valueItems = new() { valueItem });
+        return valueItems;
+    }
+
+    public static T[] Collect<T>(params T[] items) => items;
+}
---
+++ SourceCrafter.Extensions.Roslyn/RoslynExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SourceCrafter;
+
+public static class RoslynExtensions
+{
+    public static bool IsNullable(this ITypeSymbol typeSymbol) =>
+        typeSymbol is { NullableAnnotation: NullableAnnotation.Annotated } or INamedTypeSymbol { Name: "Nullable" };
+
+    public static bool AllowsNull(this ITypeSymbol typeSymbol) =>
+        typeSymbol is
+        {
+            IsValueType: false,
+            IsTupleType: false
+        } || typeSymbol.IsNullable();
+
+
+    public static bool TryGetNameOfFromAttributeArg<TSymbol>(AttributeData attr, SemanticModel model, int index, out TSymbol symbol, Func<ISymbol, bool>? predicate = null)
+        where TSymbol : class, ISymbol
+    {
+        return null != (symbol = attr.ApplicationSyntaxReference?.GetSyntax() is AttributeSyntax
+        {
+            ArgumentList.Arguments: { Count: { } count } args
+        } && count > index && // It's inside the range
+        args[index].Expression is InvocationExpressionSyntax
+        {
+            Expression: SimpleNameSyntax { Identifier.Text: "nameof" }, // Is nameof
+            ArgumentList.Arguments: [{ Expression: { } expr }] // Has just one argument
+        }
+                ? model.GetSymbolInfo(expr) switch // Contains symbol
+                {
+                    { Symbol: TSymbol foundSymbol } => foundSymbol, // Directly found
+                    {
+                        CandidateSymbols: { Length: > 0 } symbols,
+                        CandidateReason: CandidateReason.Ambiguous | CandidateReason.MemberGroup
+                    } =>
+                      (symbols.FirstOrDefault(predicate ?? IsTSymbol) as TSymbol)!, // The first one meeting the requirements
+                    _ => default!
+                }
+                : default!);
+
+        static bool IsTSymbol(ISymbol symbol) => symbol is TSymbol;
+    }
+
+}
